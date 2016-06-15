49891883efb2e0fb5d39167bd8798588e4d63796
Ben Weber <bgweber@gmail.com>
2016-06-15 15:54:17 -0700

    Update Rout.php

--- www/Rout.php
+++ www/Rout.php
@@ -54,22 +54,8 @@ else {
 	echo "Log file not found: " . $path;
  }
  else {
- 	$handle = fopen($path, "r");
-	if ($handle) {
-		while (($line = fgets($handle)) !== false) {
-			// process the line read.
-			$line = trim($line);
-			echo "$line" . "<br>";
-
-			if ($line == "user  system elapsed" || $line == "Execution halted" || explode(":", $line)[0] == "Fatal error") {
-				$running = 0;
-			}
-		}
-
-		if($running > 0) {
-			echo "<br><A HREF=\"javascript:history.go(0)\">Refresh</A>";
-		}
-	}
+	echo "Path: " . $path;
+	
 }
 ?>
 
