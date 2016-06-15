ddbd453296c801385ad54b765298a2d6d6048d0c
Ben Weber <bgweber@gmail.com>
2016-06-15 16:17:35 -0700

    Delete Compile.bat

--- Compile.bat
+++
@@ -1,8 +0,0 @@
-cd src
-javac -cp ../lib/javax.mail.jar;../lib/sigar.jar;../lib/jna.jar;../lib/jna-plat.jar;. com/ea/rserver/*.java
-jar cf rserver.jar com/ea/rserver/*.class
-cd ..
-cd ..
-mkdir bin
-xcopy /Y RServer\lib\* bin\* 
-xcopy /Y RServer\src\*.jar bin\*.jar 
