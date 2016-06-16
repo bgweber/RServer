7fc0674c41d0c4659baea5e3a4113c5b3583a458
Ben Weber <bgweber@gmail.com>
2016-06-15 17:22:45 -0700

    Update Main.R

--- tasks/ShinyMonitor/Main.R
+++ tasks/ShinyMonitor/Main.R
@@ -18,14 +18,9 @@ library(xts)
 args <- commandArgs(TRUE)  
 if (length(args) > 0) {  
   
-  # get the IP address 
-  x <- system("ipconfig", intern=TRUE)
-  z <- x[grep("IPv4", x)] 
-  server <- gsub(".*? ([[:digit:]])", "\\1", z)[1]
-  
   # get the port 
   port <- args[1]   
-  runApp(port=as.integer(port), launch.browser = FALSE, host=server)
+  runApp(port=as.integer(port))
   
 } else {
   runApp()
