fad0d924dda250d8324f4afe90175665b51a239b
Ben Weber <bgweber@gmail.com>
2016-06-16 09:22:06 -0700

    Update BuildReports.r

--- tasks/RServerTasks/BuildReports.r
+++ tasks/RServerTasks/BuildReports.r
@@ -7,5 +7,10 @@ for (lib in libraries) {
 } 
 
 require(rmarkdown)      
-render("TaskReport.Rmd", output_format = "html_document", output_file = "RServerTasks.html")      
-file.copy("RServerTasks.html", "C:/wamp/www/RServer/reports/RServer/RServerTasks.html", overwrite = TRUE) 
+render("TaskReport.rmd", output_format = "html_document", output_file = "RServerTasks.html")      
+
+if (Sys.info()['sysname'] == "Windows") {  
+  file.copy("RServerTasks.html", "C:/wamp/www/RServer/reports/RServer/RServerTasks.html", overwrite = TRUE)     
+} else {
+  file.copy("RServerTasks.html", "/var/www/html/RServer/reports/RServer/RServerTasks.html", overwrite = TRUE)      
+}
