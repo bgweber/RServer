b9f8b642e12148dc2b31df958a3ab4203fa81a26
Ben Weber <bgweber@gmail.com>
2016-06-20 10:46:11 -0700

    Update error.r

--- tasks/userDemo/error.r
+++ tasks/userDemo/error.r
@@ -1,2 +1,10 @@
 warning("This is a warning!")
+
+tryCatch({ 
+  stop("This is an error!")
+}, error = function(cond) {
+  message("Caught the error.")
+})   
+
 stop("This is an error!")
+print("Reached end of script!")
